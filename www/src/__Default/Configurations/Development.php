56dc6731218a82bdbe0c76167ffecc0df8bf2723
Gamer4647 <65735446+Gamer4647@users.noreply.github.com>
2020-06-02 16:16:00 +0100

    development.php
    development.php

---
+++ www/src/__Default/Configurations/Development.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * karting — An emulated game server for ModNation®Racers
+ *           and LittleBigPlanet™Karting on the PS3®.
+ *           © 2018-2019 Jonathan (“Jon”, @llnwn) and contributors…
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ **/
+declare(strict_types = 1);
+const SUCCESSFUL_RESPONSE_CODE = '0'; // === (string)
+const SUCCESSFUL_COMPLETION = 'Successful completion';
+const LANGUAGE_CODE = 'en-us';
+const CLIENT_TIMEZONE = -300;
+const REGION_CODE = 'scea';
+const COOKIE_DOMAIN = '';
+const IP_ADDRESS = '::1';
